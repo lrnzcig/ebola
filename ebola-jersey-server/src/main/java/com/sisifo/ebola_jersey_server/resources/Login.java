e452d42a2eee152edbb629c2f2c136836678f089
lrnzcig <lrnzcig@gmail.com>
2015-09-15 20:18:43 +0200

    list of apps as an output of login server

--- ebola-jersey-server/src/main/java/com/sisifo/ebola_jersey_server/resources/Login.java
+++ ebola-jersey-server/src/main/java/com/sisifo/ebola_jersey_server/resources/Login.java
@@ -9,15 +9,23 @@ import javax.ws.rs.core.SecurityContext;
 
 import com.sisifo.ebola_jersey_server.exception.EbolaAuthenticationException;
 
+import xre.EbolaAppsList;
+
 @Path("login")
 public class Login {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getUserName(@Context SecurityContext securityContext) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public EbolaAppsList getListOfApps(@Context SecurityContext securityContext) {
     	if (securityContext.getUserPrincipal() == null) {
     		throw new EbolaAuthenticationException("login body method");
     	}
-        return securityContext.getUserPrincipal().getName();
+    	// returns list of apps
+    	String[] apps = new String[3];
+     	apps[0] = "Bubble breaker";
+    	apps[1] = "Candy swipe slots";
+    	apps[2] = "Blood zombie";
+    	EbolaAppsList l = new EbolaAppsList(apps);
+        return l;
     }
 }
---
+++ ebola-xre/src/main/java/xre/EbolaAppsList.java
@@ -0,0 +1,26 @@
+package xre;
+
+public class EbolaAppsList {
+	
+	private String[] apps;
+
+	public EbolaAppsList() {
+		super();
+	}
+
+	public EbolaAppsList(String[] apps) {
+		super();
+		this.setApps(apps);
+	}
+
+	public String[] getApps() {
+		return apps;
+	}
+
+	public void setApps(String[] apps) {
+		this.apps = apps;
+	}
+	
+	
+
+}
